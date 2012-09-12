092df035cbea5bfdbf164d44d89ebbc1903feb17
Hugo-Fontaine <weedmaster24@hotmail.com>
2012-09-12 14:39:00 -0400

    Header CSS et images biatch
    Signed-off-by: Hugo-Fontaine <weedmaster24@hotmail.com>
    
    Signed-off-by: Hugo-Fontaine <weedmaster24@hotmail.com>

--- Synthese/Web/partial/header.php
+++ Synthese/Web/partial/header.php
@@ -1,14 +1,12 @@
-<?php
-/* ---------------------------------------------------
+<!--/* ---------------------------------------------------
 * *
 * Projet synthèse : H2012 B63*
 * Fait Par : Etienne Belanger,Hugo Fontaine*
 * *
-*--------------------------------------------------- */
-?>
+*--------------------------------------------------- */-->
 
-
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"><html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 		<head>
 		<meta http-equiv="imagetoolbar" content="no" />
         <link href="css/global.css" rel="stylesheet" type="text/css" media="screen" />
@@ -16,33 +14,44 @@
 		<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
 	    </head>
     <body>
-	<a href="index.php">Acceuil</a>
-	
-	<a href="services.php">Service</a>
-	<a href="career.php">Carrières</a>
-	<a href="gallery.php">Gallery</a>
-	<a href="company.php">Compagnie</a>
-	<a href="contact.php">Contact</a>
-	Bonjour, <?php echo $action->getName(); ?>
-	<?php
-		if(!$action->isConnected())
-			{
-		?>
-			<a href="login.php">Login</a>
-			<a href="register.php">S'Enregistrer</a>
-	<?php
-			}
-			else
-			{
-		?>
-			<a href="?logout=true">Déconnexion</a>
-		<?php
-			if($action->isAdmin())
-			  {
-			?>
-				<a href="admin.php">Administrateur</a>
-			<?php
-			  }
-			}
-		
-		
+		<div class="site-container">
+		    <div class="page-container">
+		    	<div class="site-header">
+		    		<div class="page-title-section">
+			    		<h1><span>DynamiKoncept</span></h1>			    		
+		    		</div>
+		    		<div class="menu-section">
+		    			<ul>
+		    				<li><a href="index.php">Acceuil</a></li>
+		    				<li><a href="services.php">Service</a></li>
+		    				<li><a href="career.php">Carrières</a></li>
+							<li><a href="gallery.php">Gallery</a></li>
+							<li><a href="company.php">Compagnie</a></li>
+							<li><a href="contact.php">Contact</a></li>
+							<li>Bonjour, <?php echo $action->getName(); ?></li>
+								<?php
+								if(!$action->isConnected())
+								{
+								?>
+								<li><a href="login.php">Login</a></li>
+								<li><a href="register.php">S'Enregistrer</a></li>
+								<?php
+								}
+								else
+								{
+								?>
+									<li><a href="?logout=true">Déconnexion</a></li>
+								<?php
+									if($action->isAdmin())
+									{
+								?>
+									<li><a href="admin.php">Administrateur</a>>/li>
+								<?php
+									}
+								}
+								?>
+		    			</ul>
+		    		</div>
+		    		<div class="clear"></div>
+		    	</div>
+		    	<div class="page-content">
